c20c9d82919181e66d35eca43addb7af267d8452
Superhero5D <58561653+Superhero5D@users.noreply.github.com>
2021-10-21 09:47:33 -0400

    Bare Bones of project

---
+++ bricksbroken.py
@@ -0,0 +1,16 @@
+import pygame
+def define_parameters():
+
+class Game:
+    """ Initialize the game"""
+    def __init__(self, game_width, game_height):
+        pygame.display.set_caption('BrickBreaker')
+        self.game_width = game_width
+        self.game_height = game_height
+        self.gameDisplay = pygame.display.set_mode((game_width, game_height))
+        self.player = Player(self)
+        self.bricks = Bricks()
+        self.ball = Ball()
+        self.score = 0
+
+class Player(object):
